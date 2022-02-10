e67968dce0c42312f9fc1c10dd5a2ef86075b6f2
DaanBakker <daan.bakker@infocaster.net>
2022-02-10 20:57:28 +0100

    rename wpfapp to wpfserver

--- WpfApp/ServerApp.xaml.cs
+++ WpfApp/ServerApp.xaml.cs
@@ -10,7 +10,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using WpfAppServer;
 
-namespace WpfApp
+namespace WpfServer
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
